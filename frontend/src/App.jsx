8582f282ecdd1b406e207fde78d7eda69fb05375
nocassage <alexandracosta115@gmail.com>
2025-05-28 16:52:25 +0100

    pagina

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Profile from './pages/Profile/Profile'
+import Alarms from './pages/Alarms/Alarms'
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={Profile} />
+        <Route path="/gir" element={Profile} />
+        <Route path="/alarms" element={Alarms} />
       </Routes>
     </Router>
   )
