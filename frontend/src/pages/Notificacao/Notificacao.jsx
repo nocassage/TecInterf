c714b29cc34fdc17b1604b307e57242bbecdf2b1
nocassage <alexandracosta115@gmail.com>
2025-06-02 17:17:35 +0100

    notif

---
+++ frontend/src/pages/Notificacao/Notificacao.jsx
@@ -0,0 +1,18 @@
+import "./Notificacao.css"
+import micro from "../../assets/images/micro.png"
+
+const Notificacao = () => {
+    return (
+        <div className="notif-page">
+          <div className="hora">
+            <h1>17:30</h1>
+            <h2>Suplemento</h2>
+           </div>
+           <button class="image-button" onclick="alert('Fale agora!')">
+        <img src={micro} alt="Botão Imagem" className="micro-image"/>
+            </button>
+        </div>
+    )
+}
+
+export default Notificacao
