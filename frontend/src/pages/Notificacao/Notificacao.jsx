5cda5cce8b683cae26f8dda84ed111837ab3a230
sofia santos <sofia.parreira.santos@gmail.com>
2025-06-02 19:35:32 +0100

    FIXES

--- frontend/src/pages/Notificacao/Notificacao.jsx
+++ frontend/src/pages/Notificacao/Notificacao.jsx
@@ -4,12 +4,12 @@ import micro from "../../assets/images/micro.png"
 const Notificacao = () => {
     return (
         <div className="notif-page">
-          <div className="hora">
-            <h1>17:30</h1>
-            <h2>Suplemento</h2>
-           </div>
-           <button class="image-button" onclick="alert('Fale agora!')">
-        <img src={micro} alt="Botão Imagem" className="micro-image"/>
+            <div className="hora">
+                <h1>17:30</h1>
+                <h2>Suplemento</h2>
+            </div>
+            <button class="image" onclick="alert('Fale agora!')">
+                <img src={micro} alt="Botão Imagem" className="micro-image" />
             </button>
         </div>
     )
