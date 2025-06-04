f2e39ae9a26ee36e4ad89247bbb6b4d8bb910b8d
Denecio <vascopereirabastos@gmail.com>
2025-06-04 17:02:51 +0100

    finaly

--- backend/server.js
+++ backend/server.js
@@ -9,18 +9,20 @@ const port = 3001
 app.use(cors())
 app.use(express.json())
 
+
 // Update this to match your actual COM port or /dev/tty*
 const arduinoPort = new SerialPort({
-  path: '/dev/tty.usbmodem21101', // e.g., '/dev/ttyUSB0' on Linux
+  path: '/dev/tty.usbmodem21401', // e.g., '/dev/ttyUSB0' on Linux
   baudRate: 9600,
 })
 
 const parser = arduinoPort.pipe(new ReadlineParser({ delimiter: '\n' }));
 
+let alarmState = { triggered: false }
 // Print Arduino output to the Node.js console
 parser.on('data', (line) => {
-  console.log('[Arduino]', line.trim());
-});
+  console.log('[Arduino]', line.trim())
+})
 
 arduinoPort.on('open', () => {
   console.log('Serial connection to Arduino opened.')
